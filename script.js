237226c81e15b3f5a971756def6ded0327fccd9e
cam5674 <cameronlodge93@gmail.com>
2025-09-29 20:10:02 -0700

    completed getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Hello world!")
+
+function getCumpterChoice() {
+    // get random number 0-2
+    randomNumber = Math.floor(Math.random() * 3)
+    console.log(randomNumber)
+
+    // map numbers to choices
+    if (randomNumber === 0) {
+        return "Rock"
+    } else if (randomNumber === 1) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+
+}
+console.log (getCumpterChoice())
